0249bce1c05b51296c0e87a6536a02f0fb25b8ec
Administrator <Administrator@RHJBSIOQNK3K8PU>
2015-02-07 22:56:08 +0800

    tt

--- javaBase/src/main/java/org/fxi/test/javaBase/java8/util/stream/StreamTest.java
+++ javaBase/src/main/java/org/fxi/test/javaBase/java8/util/stream/StreamTest.java
@@ -1,10 +1,14 @@
 package org.fxi.test.javaBase.java8.util.stream;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class StreamTest {
 	@Test
 	public void test001(){
-		
+		List<String> names = new ArrayList<String>();
+		names.stream().filter(str -> str.startsWith("f")).mapToInt(str -> 1).sum();
 	}
 }	
